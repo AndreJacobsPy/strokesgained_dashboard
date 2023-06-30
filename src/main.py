04b3469c25ce64a6232bf0b160838cf75560407d
AndreJacobsPy <andrejacobs365@gmail.com>
2023-06-30 13:05:00 -0500

    first commit

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+import streamlit as st
+import sqlalchemy as pgsql
+import pandas as pd
+
+from hidden import URL
+
+def app() -> None:
+    st.markdown('# US Open 2023 Dashboard')
 
 
+if __name__ == "__main__":
+    app()
+    
