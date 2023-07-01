c085af0ae78bc25f3692be9a0ea5db9d1bd661fb
AndreJacobsPy <andrejacobs365@gmail.com>
2023-06-30 21:52:20 -0500

    trying database

--- src/main.py
+++ src/main.py
@@ -7,7 +7,10 @@ from hidden import URL
 def app() -> None:
     st.markdown('# US Open 2023 Dashboard')
 
+    conn = pgsql.create_engine(URL)
+    data = conn.execute("select * from strokesgained limit 5").fetchall()
+    st.dataframe(data)
+
 
 if __name__ == "__main__":
     app()
-    
