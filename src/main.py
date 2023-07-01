09c7c486ae66b1ae108b4c4b7003e0a181d2b670
AndreJacobsPy <andrejacobs365@gmail.com>
2023-06-30 22:32:07 -0500

    connect database

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 import streamlit as st
+import sqlalchemy as pgsql
+import pandas as pd
+
+from hidden import URL
+
+@st.cache_data
+def query():
+    # connect database
+    with pgsql.create_engine(URL).connect() as db:
+        query = pgsql.text('select * from strokesgained limit 5')
+        data = db.execute(query)
+        df = pd.DataFrame(data)
+
+        return df
 
 
 def app() -> None:
     st.title('Us Open Dashboard')
+
+    st.markdown('## My Data')
+    st.dataframe(query())
+    
+
 
 
 if __name__ == "__main__":
