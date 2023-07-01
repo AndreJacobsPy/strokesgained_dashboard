ad99f56320e281de586e9465bb94f48d16c37732
AndreJacobsPy <andrejacobs365@gmail.com>
2023-06-30 21:55:32 -0500

    fix

--- src/main.py
+++ src/main.py
@@ -7,9 +7,8 @@ from hidden import URL
 def app() -> None:
     st.markdown('# US Open 2023 Dashboard')
 
-    conn = pgsql.create_engine(URL)
-    data = conn.execute("select * from strokesgained limit 5").fetchall()
-    st.dataframe(data)
+    db = pgsql.create_engine(URL)
+    data = db.execute("select * from strokesgained limit 5")
 
 
 if __name__ == "__main__":
